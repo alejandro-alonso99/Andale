f4a98f2df200d1502b242070c017c8b5bd4c541f
Alequita <alejandroalonso0306@gmail.com>
2022-04-28 11:16:50 -0300

    add staticfiles config

--- andale/settings.py
+++ andale/settings.py
@@ -133,6 +133,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 LOGIN_REDIRECT_URL = 'products:products_list'
 LOGIN_URL = 'account:login'
 
